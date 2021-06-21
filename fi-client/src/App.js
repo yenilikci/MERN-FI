1473605898b4a5513a437c9f5612ca5228cfc9a1
yenilikci <m.melihcelik017@outlook.com>
2021-06-21 20:25:08 +0300

    form validasyonunun sağlanması ve gönderim sonrası alanların temizlenmesi

--- fi-client/src/App.js
+++ fi-client/src/App.js
@@ -3,18 +3,33 @@ import axios from "axios";
 import "./App.css";
 
 export default class App extends Component {
+  
   state = {
     tamIsim: "",
     emailAdres: "",
     fikirTuru: "Öneri",
     fikir: "",
     alertSuccessDisplay: "none",
-    alertErrorDisplay: "none"
+    alertErrorDisplay: "none",
   };
 
+  alanlariTemizle = () => {
+    //const { tamIsim, emailAdres, fikirTuru, fikir } = this.state
+    this.setState({tamIsim:'',emailAdres:'',fikirTuru:'Öneri',fikir:''})
+  }
+
+  formValidation = () => {
+    const { tamIsim, emailAdres, fikirTuru, fikir } = this.state
+    if(!tamIsim||!emailAdres||!fikirTuru||!fikir) return false
+    return true
+  }
+  
   formuGonder = () => {
     //console.log(this.state);
-    const { tamIsim, emailAdres, fikirTuru, fikir } = this.state;
+    const { tamIsim, emailAdres, fikirTuru, fikir } = this.state
+    
+    //doğrulamayı geçerse
+    if(!this.formValidation()) return
     axios
       .post("http://localhost:5555/fikirkaydet", {
         tamIsim,
@@ -23,50 +38,60 @@ export default class App extends Component {
         fikir,
       })
       .then((res) => {
-        this.setState({alertSuccessDisplay:'block'})
-      }).catch(err => {
-        this.setState({alertErrorDisplay:'block'})
+        this.alanlariTemizle()
+        this.setState({ alertSuccessDisplay: "block" });
       })
+      .catch((err) => {
+        this.setState({ alertErrorDisplay: "block" });
+      });
   };
 
   render() {
     return (
-      <div class="text-center mt-3">
+      <div className="text-center mt-3">
         <h1>FI APP</h1>
         <div className="mx-auto w-25 mt-3">
-          <div className="alert alert-success" role="alert" style={{display:this.state.alertSuccessDisplay}}>
+          <div
+            className="alert alert-success"
+            role="alert"
+            style={{ display: this.state.alertSuccessDisplay }}
+          >
             Fikriniz iletilmiştir!
           </div>
 
-          <div className="alert alert-danger" role="alert" style={{display:this.state.alertErrorDisplay}}>
+          <div
+            className="alert alert-danger"
+            role="alert"
+            style={{ display: this.state.alertErrorDisplay }}
+          >
             Fikriniz iletilmiştir!
           </div>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Ad Soyad</label>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlInput1">Ad Soyad</label>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               id="exampleFormControlInput1"
               placeholder="Ad Soyad"
               value={this.state.tamIsim}
               onChange={(e) => this.setState({ tamIsim: e.target.value })}
             />
           </div>
-          <div class="form-group my-3">
-            <label for="exampleFormControlInput2">Email Adresi</label>
+          <div className="form-group my-3">
+            <label htmlFor="exampleFormControlInput2">Email Adresi</label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               id="exampleFormControlInput2"
               placeholder="name@example.com"
               value={this.state.emailAdres}
               onChange={(e) => this.setState({ emailAdres: e.target.value })}
             />
           </div>
-          <div class="form-group m-3">
-            <label for="exampleFormControlSelect1">Fikir Türü</label>
+          <div className="form-group m-3">
+            <label htmlFor="exampleFormControlSelect1">Fikir Türü</label>
             <select
-              class="form-control"
+              className="form-control"
               id="exampleFormControlSelect1"
               onChange={(e) => this.setState({ fikirTuru: e.target.value })}
             >
@@ -75,10 +100,10 @@ export default class App extends Component {
             </select>
           </div>
 
-          <div class="form-group m-3">
-            <label for="exampleFormControlTextarea1">İçerik</label>
+          <div className="form-group m-3">
+            <label htmlFor="exampleFormControlTextarea1">İçerik</label>
             <textarea
-              class="form-control"
+              className="form-control"
               id="exampleFormControlTextarea1"
               rows="3"
               value={this.state.fikir}
