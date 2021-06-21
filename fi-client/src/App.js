b5b7dd1ec119cb8b78ee919b39b35e08c33e6087
yenilikci <m.melihcelik017@outlook.com>
2021-06-21 18:38:01 +0300

    state yapılandırılması, input elemanlarında onChange ile birlikte statelerin güncellenmesi

--- fi-client/src/App.js
+++ fi-client/src/App.js
@@ -3,22 +3,23 @@ import axios from "axios";
 import "./App.css";
 
 export default class App extends Component {
-  componentDidMount() {
-    this.kullaniciCek();
+
+  state = {
+    tamIsim:'',
+    emailAdres:'',
+    fikirTuru:'',
+    fikir:''
   }
 
-  kullaniciCek = () => {
-    axios
-      .get("http://localhost:5555/kullanici")
-      .then((res) => console.log(res.data));
-  };
+  formuGonder = () => {
+    console.log(this.state);
+  }
 
   render() {
     return (
       <div class="text-center mt-3">
         <h1>FI APP</h1>
         <div className="mx-auto w-25 mt-3">
-          <form>
             <div class="form-group">
               <label for="exampleFormControlInput1">Ad Soyad</label>
               <input
@@ -26,6 +27,8 @@ export default class App extends Component {
                 class="form-control"
                 id="exampleFormControlInput1"
                 placeholder="Ad Soyad"
+                value={this.state.tamIsim}
+                onChange={e => this.setState({tamIsim:e.target.value})}
               />
             </div>
             <div class="form-group my-3">
@@ -35,11 +38,15 @@ export default class App extends Component {
                 class="form-control"
                 id="exampleFormControlInput2"
                 placeholder="name@example.com"
+                value={this.state.emailAdres}
+                onChange={e => this.setState({emailAdres:e.target.value})}
               />
             </div>
             <div class="form-group m-3">
-              <label for="exampleFormControlSelect1">Kategori</label>
-              <select class="form-control" id="exampleFormControlSelect1">
+              <label for="exampleFormControlSelect1">Fikir Türü</label>
+              <select class="form-control" id="exampleFormControlSelect1"
+              onChange={e => this.setState({fikirTuru:e.target.value})}
+              >
                 <option>Öneri</option>
                 <option>Hata Bildir</option>
               </select>
@@ -51,12 +58,13 @@ export default class App extends Component {
                 class="form-control"
                 id="exampleFormControlTextarea1"
                 rows="3"
+                value={this.state.fikir}
+                onChange={e => this.setState({fikir:e.target.value})}
               ></textarea>
             </div>
-            <button className="btn btn-success">
+            <button className="btn btn-success" onClick={this.formuGonder}>
               Gönder
             </button>
-          </form>
         </div>
       </div>
     );
