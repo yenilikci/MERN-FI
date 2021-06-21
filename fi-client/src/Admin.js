65b571a2053c82ed4621cc63e1ea1edca913fd9d
yenilikci <m.melihcelik017@outlook.com>
2021-06-21 22:30:05 +0300

    client tarafında Admin componentinin içeriği düzenlendi

--- fi-client/src/Admin.js
+++ fi-client/src/Admin.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
 
 export default class Admin extends Component {
+
+    state = {
+        username:'',
+        password:''
+    }
+
     render() {
         return (
-            <div>
-                <h1>Admin Panel</h1>
+            <div className="text-center mt-3">
+            <h1>Admin Giriş</h1>
+            <div className="mx-auto w-25 mt-3">
+              <div className="form-group">
+                <label htmlFor="exampleFormControlInput1">Kullanıcı Adı</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Kullanıcı Adı"
+                  value={this.state.username}
+                  onChange={(e) => this.setState({ username: e.target.value })}
+                />
+              </div>
+              <div className="form-group my-3">
+                <label htmlFor="exampleFormControlInput2">Şifre</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleFormControlInput2"
+                  placeholder="******"
+                  value={this.state.password}
+                  onChange={(e) => this.setState({ password: e.target.value })}
+                />
+              </div>
+              <button className="btn btn-success w-100" onClick={this.formuGonder}>
+                Giriş
+              </button>
             </div>
+            <br/>
+          </div>
         )
     }
 }
